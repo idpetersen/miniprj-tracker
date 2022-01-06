7be7fcf7a95a559483314aab4ccf785927f1e32a
isaac petersen <isaac.petersen5@gmail.com>
2022-01-06 11:50:22 -0800

    wip

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-<<<<<<< HEAD
+
 var saveChanges = $('#saveChanges')
 //Added event listener to Add project button
 // $( "#button" ).on( "click", function() {
 //     console.log( "hi" );
 //   });
 
-=======
+
 //Showing Date and time
->>>>>>> 4d18d210adeb990245d5fa64531c587b263d506e
 window.setInterval(function (){
     $('#currentDay').html(moment().format('ddd MM/DD/y H:mm:ss'))
 }, 1000);
 
-<<<<<<< HEAD
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
 
 console.log(saveChanges)
-=======
-//jQuery UI's datepicker with a minimum date setting in place
->>>>>>> 4d18d210adeb990245d5fa64531c587b263d506e
+
