456f9ce2e7fc8e23d74a3cd86135a2d73d9fa0fd
sinka27 <sinka27392@gmail.com>
2022-01-06 10:31:58 -0800

    comment

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //form
-$(document).ready(function() {
-    $("button").click(function() {
-        var x = $("form").serializeArray();
-        $.each(x, function(i, field) {
-            $("#output").append(field.name + ":"
-                    + field.value + " ");
-        });
-    });
-});
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         var x = $("form").serializeArray();
+//         $.each(x, function(i, field) {
+//             $("#output").append(field.name + ":"
+//                     + field.value + " ");
+//         });
+//     });
+// });
